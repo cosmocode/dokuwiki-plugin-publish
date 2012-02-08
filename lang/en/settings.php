f062390165b91da58693d3aced67009122ecdcf3
Andreas Gohr <andi@splitbrain.org>
2012-02-08 12:28:13 +0100

    wording fix

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['apr_namespaces'] = 'Namespaces this plugin applies to';
-$lang['number_of_approved'] = 'Number of users needed to approve a document.';
+$lang['apr_namespaces'] = 'Namespaces this plugin applies to.';
+$lang['number_of_approved'] = 'Number of users needed to approve a page.';
