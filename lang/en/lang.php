9fd92cc2f785250faf80ef9f5df5420b9bb03391
Andreas Gohr <andi@splitbrain.org>
2012-02-08 12:29:57 +0100

    spelling fixes

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,14 +1,14 @@
 <?php
 
 // banner
-$lang['apr_recent_draft'] = 'The <a href="%s">Most Recent Version</a> of this document is a draft.';
-$lang['apr_outdated'] = 'This version is <em>Outdated</em> by a <a href="%s">Newer Approved Version</a>.';
-$lang['apr_draft'] = 'This version (%s) is a <em>Draft</em>.';
-$lang['apr_approved'] = 'This version (%s) was <em>Approved</em> by %s.';
-$lang['apr_previous'] = 'The <a href="%s">Previously Approved Version</a> (%s) is available.';
+$lang['apr_recent_draft'] = 'The <a href="%s">most recent version</a> of this page is a draft.';
+$lang['apr_outdated'] = 'This version is <em>outdated</em> by a <a href="%s">newer approved version</a>.';
+$lang['apr_draft'] = 'This version (%s) is a <em>draft</em>.';
+$lang['apr_approved'] = 'This version (%s) was <em>approved</em> by %s.';
+$lang['apr_previous'] = 'The <a href="%s">Previously approved version</a> (%s) is available.';
 
 // Pending Approvals List
-$lang['apr_p_none'] = 'No Documents Pending Approval';
+$lang['apr_p_none'] = 'No documents pending approval';
 $lang['apr_p_hdr_page'] = 'Page';
 $lang['apr_p_hdr_previous'] = 'Previous Approval';
 $lang['apr_p_hdr_updated'] = 'Updated';
