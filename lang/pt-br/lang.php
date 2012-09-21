5f15b424f15bd0ac805de5b631f0c0992d205761
Benedito Carneiro <benedito.carneiro@gmail.com>
2012-09-21 13:04:16 +0200

    Brazilian Portuguese translation

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,20 @@
+<?php
+
+// banner
+$lang['apr_recent_draft'] = 'A <a href="%s">Vers&atilde;o Mais Recente</a> deste documento &eacute; um rascunho.';
+$lang['apr_outdated'] = 'Esta vers&atilde;o foi <em>Substitu&iacute;da</em> por uma <a href="%s">Vers&atilde;o Recentemente Aprovada</a>.';
+$lang['apr_draft'] = 'Esta vers&atilde;o (%s) &eacute; um <em>Rascunho</em>.';
+$lang['apr_approved'] = 'Esta vers&atilde;o (%s) foi <em>Aprovada</em> por <em>%s</em>.';
+$lang['apr_previous'] = ' A <a href="%s">Vers&atilde;o Anteriormente Aprovada</a> (%s) est&aacute; dispon&iacute;vel.';
+
+// Pending Approvals List
+$lang['apr_p_none'] = 'N&atilde;o h&aacute; documentos pendentes de aprova&ccedil;&atilde;o';
+$lang['apr_p_hdr_page'] = 'P&aacute;gina';
+$lang['apr_p_hdr_previous'] = 'Aprova&ccedil;&atilde;o Anterior';
+$lang['apr_p_hdr_updated'] = 'Atualizado';
+$lang['apr_p_approved'] = 'Por %s em <a href="%s">%s</a>';
+
+// Submit (Next to the tick-box to approve a page)
+$lang['apr_do_approve'] = 'Aprovar';
+
+?>
