c8d2138308b2bdc729b6e532bd39a8271e9220b6
Christopher Smith <chris@jalakai.co.uk>
2013-02-20 17:37:29 +0000

    add explanation of how to include multiple namespaces in the (en) settings (will need added for other languages)

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['apr_namespaces'] = 'Namespaces this plugin applies to.';
-$lang['no_apr_namespaces'] = 'Namespaces this plugin <strong>does not</strong> apply to.';
+$lang['apr_namespaces'] = 'Namespaces this plugin applies to (space separated list).';
+$lang['no_apr_namespaces'] = 'Namespaces this plugin <strong>does not</strong> apply to (space separated list).';
 $lang['number_of_approved'] = 'Number of users needed to approve a page.';
 $lang['hidereaderbanner'] = 'Hide banner to read only users';
 $lang['hide drafts'] = 'Hide drafts to read only users';
