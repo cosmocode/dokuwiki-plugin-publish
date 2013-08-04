1c91c9397648ae7a8539de59b0aff62952951f45
Aleksandr Selivanov <alexgearbox@gmail.com>
2013-08-04 16:20:18 +0200

    translation update

---
+++ lang/ru/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Aleksandr Selivanov <alexgearbox@gmail.com>
+ */
+$lang['apr_namespaces']        = 'Пространства имён, к которым применяется плагин';
+$lang['apr_approved_text']     = 'Автоматический комментарий об одобрении';
