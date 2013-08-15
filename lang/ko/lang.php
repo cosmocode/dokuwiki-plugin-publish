bd1a3418dab54e1be76ac66c36d50b2e5be99745
Myeongjin <aranet100@gmail.com>
2013-08-15 05:50:24 +0200

    translation update

---
+++ lang/ko/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['apr_recent_draft']      = '이 문서의 <a href="%s">가장 최근 판</a>은 초안입니다.';
+$lang['apr_outdated']          = '이 판은 <a href="%s">새 승인된 판</a>에 의해 <em>오래되었습니다</em>.';
+$lang['apr_draft']             = '이 판(%s)은 <em>초안</em>입니다.';
+$lang['apr_approved']          = '이 판(%s)은  %s 사용자에 의해 <em>승인되었습니다</em>.';
+$lang['apr_previous']          = ' <a href="%s">이전에 승인된 판</a>(%s)은 사용할 수 있습니다.';
+$lang['apr_p_none']            = '승인 없이 보류 중인 문서 없음';
+$lang['apr_p_hdr_page']        = '문서';
+$lang['apr_p_hdr_previous']    = '승인 없이 보류 중';
+$lang['apr_p_hdr_updated']     = '업데이트됨';
+$lang['apr_p_approved']        = '승인자 %s 장소 <a href="%s">%s</a>';
+$lang['apr_do_approve']        = '승인';
