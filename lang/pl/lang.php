9b1fd29c0811129e0b8b2ca8d9998e566a0a5699
Szymon Olewniczak <szymon.olewniczak@rid.pl>
2013-09-26 11:08:31 +0200

    polish translation added

---
+++ lang/pl/lang.php
@@ -0,0 +1,20 @@
+<?php
+
+// banner
+$lang['apr_recent_draft'] = '<a href="%s">Najnowsza wersja</a> tego dokumentu jest niezatwierdzona.';
+$lang['apr_outdated'] = 'Ta wersja strony została <em>zdezaktualizowana</em> przez <a href="%s">nową zatwierdzoną werjsę</a>.';
+$lang['apr_draft'] = 'Ta wersja strony (%s) jest <em>niezatwierdzona</em>.';
+$lang['apr_approved'] = 'Ta wersja strony(%s) została <em>zatwierdzona</em> przez %s.';
+$lang['apr_previous'] = 'Dostępna <a href="%s">poprzednio zatwierdzona wersja</a> (%s).';
+
+// Pending Approvals List
+$lang['apr_p_none'] = 'Brak dokumentów oczekujących na zatwierdzenie';
+$lang['apr_p_hdr_page'] = 'Nazwa strony';
+$lang['apr_p_hdr_previous'] = 'Ostatnia zatwierdzona wersja';
+$lang['apr_p_hdr_updated'] = 'Data ostatniej zmiany';
+$lang['apr_p_approved'] = 'Zatwierdzona przez %s dnia: <a href="%s">%s</a>';
+
+// Submit (Next to the tick-box to approve a page)
+$lang['apr_do_approve'] = 'Zatwierdź';
+
+?>
---
+++ lang/pl/settings.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['apr_namespaces'] = 'Namespaces this plugin applies to';
+$lang['apr_approved_text'] = 'Automatic Approval Comment';
+
+?>
