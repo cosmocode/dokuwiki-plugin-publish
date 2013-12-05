a776872c8b08b92e2efa4531a264eb8c3bb94686
Christian "Na_kai" Sueur <sueur.christian@gmail.com>
2013-12-05 13:55:05 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Christian "Na_kai" Sueur <sueur.christian@gmail.com>
+ */
+$lang['apr_recent_draft']      = 'La <a href="%s"> version la plus récente </ a> de ce document est un projet.';
+$lang['apr_outdated']          = 'Cette version est <em> expirée </ em> par une <a href="%s"> plus récente version approuvée </ a>.';
+$lang['apr_draft']             = 'Cette version (%s) est en <em> Projet </em>';
+$lang['apr_approved']          = 'Cette version (%s) à été <em>Approuvé</em> par %s.';
+$lang['apr_previous']          = 'La <a href="%s">version précédemment approuvé</a> (%s) est disponible.';
+$lang['apr_p_none']            = 'Pas de documents en attente d\'approbation';
+$lang['apr_p_hdr_page']        = 'Page';
+$lang['apr_p_hdr_previous']    = 'Approbation précédente';
+$lang['apr_p_hdr_updated']     = 'Mise à jour';
+$lang['apr_p_approved']        = 'Par %s sur <a href="%s">%s</a>';
+$lang['apr_do_approve']        = 'Accepter';
