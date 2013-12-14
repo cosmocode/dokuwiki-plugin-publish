9ddb24dbf30b38ecab89bbf344f46717b9af1f2d
Jacques Bouthier <jacques@jack31.net>
2013-12-14 10:10:04 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Jacques Bouthier <jacques@jack31.net>
+ */
+$lang['apr_recent_draft']      = 'La <a href="%s"> version la plus récente</a> de ce document est au statut brouillon.';
+$lang['apr_outdated']          = 'Cette version est <em> remplacée </em> par une <a href="%s">Nouvelle Version Validée</a>.';
+$lang['apr_draft']             = 'Cette version (%s) est un <em>brouillon</em>.';
+$lang['apr_approved']          = 'Cette version (%s) a été<em>validée</em> par%s.';
+$lang['apr_previous']          = 'La<a href="%s">version précédemment validée</a> (%s) est dis^ponible.';
+$lang['apr_p_none']            = 'Pas de documents en attente de validation';
+$lang['apr_p_hdr_page']        = 'Page';
+$lang['apr_p_hdr_previous']    = 'Précédente validation';
+$lang['apr_p_hdr_updated']     = 'Mis à jour';
+$lang['apr_p_approved']        = 'Par %s sur<a href="%s">%s</a>';
+$lang['apr_do_approve']        = 'Validé';
