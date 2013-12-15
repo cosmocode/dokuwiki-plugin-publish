baa3f1f3c799257bcda79634256961a2afcc688a
Nicolas Friedli <nicolas@theologique.ch>
2013-12-15 18:55:05 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Nicolas Friedli <nicolas@theologique.ch>
+ */
+$lang['apr_recent_draft']      = 'La <a href="%s">Version la plus récente</a> de ce document est un brouillon.';
+$lang['apr_outdated']          = 'Cette version est <em>Périmée</em> par une <a href="%s">Nouvelle version aprouvée</a>.';
+$lang['apr_draft']             = 'Cette version (%s) est un <em>Brouillon</em>.';
+$lang['apr_approved']          = 'Cette version (%s) a été <em>Aprouvée</em> par %s.';
+$lang['apr_previous']          = 'La <a href="%s">Version aprouvée précédemment</a> (%s) est disponible.';
+$lang['apr_p_none']            = 'Aucun document en attente d\'aprobation';
+$lang['apr_p_hdr_page']        = 'Page';
+$lang['apr_p_hdr_previous']    = 'Aprouvé précédemment';
+$lang['apr_p_hdr_updated']     = 'Mis à jour';
+$lang['apr_p_approved']        = 'Par %s sur <a href="%s">%s</a>';
+$lang['apr_do_approve']        = 'Aprouver';
---
+++ lang/fr/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Nicolas Friedli <nicolas@theologique.ch>
+ */
+$lang['apr_namespaces']        = 'Espaces de noms auquel ce plugin s\'applique';
+$lang['apr_approved_text']     = 'Approuver automatiquement les commentaires';
