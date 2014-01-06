9d8b75e80de931805ae01b092d29668da153c237
Samory Pereira Santos <samory.santos@gmail.com>
2014-01-06 21:45:24 +0100

    translation update

---
+++ lang/pt-br/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Samory Pereira Santos <samory.santos@gmail.com>
+ */
+$lang['apr_approved_text']     = 'Comentário autom&aacute;tico de aprovação';
