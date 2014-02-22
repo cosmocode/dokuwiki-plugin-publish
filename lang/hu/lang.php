e81eecf6264f9bf1d7e7a1083b05993a23212172
Marina Vladi <deldadam@gmail.com>
2014-02-22 12:51:54 +0100

    translation update

---
+++ lang/hu/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Marina Vladi <deldadam@gmail.com>
+ */
+$lang['apr_recent_draft']      = 'A dokumentum <a href="%s">legújabb változata</a> még piszkozat.';
+$lang['apr_outdated']          = 'Ezt a változatott <em>frissítette</em> egy <a href="%s">újonnan jóváhagyott változat</a>.';
+$lang['apr_draft']             = 'Ez a változat (%s) egy <em>piszkozat</em>.';
+$lang['apr_approved']          = 'Ezt a változatot (%s) %s <em>hagyta jóvá</em>. ';
+$lang['apr_previous']          = 'Az <a href="%s">előzőleg jóváhagyott változat</a> (%s) még elérhető.';
+$lang['apr_p_none']            = 'Nincs jóváhagyásra váró dokumentum.';
+$lang['apr_p_hdr_page']        = 'Oldal';
+$lang['apr_p_hdr_previous']    = 'Előző jóváhagyás';
+$lang['apr_p_hdr_updated']     = 'Frissítette';
+$lang['apr_p_approved']        = '%s - <a href="%s">%s</a>';
+$lang['apr_do_approve']        = 'Jóváhagyás';
---
+++ lang/hu/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Marina Vladi <deldadam@gmail.com>
+ */
+$lang['apr_namespaces']        = 'Névterek, amelyekben a bővítmény aktív';
+$lang['apr_approved_text']     = 'Automatikus jóváhagyás megjegyzése';
