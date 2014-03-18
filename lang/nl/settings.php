be6529683d1f038702efa26d6d76509516ef8bd9
Rene <wllywlnt@yahoo.com>
2014-03-18 14:30:05 +0100

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Rene <wllywlnt@yahoo.com>
+ */
+$lang['apr_namespaces']        = 'Namespaces waarop deze plugin betrekking heeft';
+$lang['apr_approved_text']     = 'Automatisch goedkeuring commentaar';
