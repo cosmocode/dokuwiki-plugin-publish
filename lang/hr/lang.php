f6865b3eeca71d595e6d80ebe14ec5430354119b
Davor Turkalj <turki.bsc@gmail.com>
2014-07-08 10:40:05 +0200

    translation update

---
+++ lang/hr/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Davor Turkalj <turki.bsc@gmail.com>
+ */
+$lang['apr_recent_draft']      = 'Zadnja <a href="%s">verzija ovog dokumenta</a> je draft.';
+$lang['apr_outdated']          = 'Ova verzija je <em>starija</em> od <a href="%s">zadnje odobrene verzije</a>.';
+$lang['apr_draft']             = 'Ova verzija (%s) je <em>Draft</em>.';
+$lang['apr_approved']          = 'Ovu verziju (%s) je <em>Odobrio</em> %s.';
+$lang['apr_previous']          = 'Raspoloživa je <a href="%s">prethodno odobrena verzija</a> (%s).';
+$lang['apr_p_none']            = 'Nema dokumenata koji čekaju odobrenje';
+$lang['apr_p_hdr_page']        = 'Stranica';
+$lang['apr_p_hdr_previous']    = 'Prethodno odobrenje';
+$lang['apr_p_hdr_updated']     = 'izmijenjena';
+$lang['apr_p_approved']        = 'Od %s na <a href="%s">%s</a>';
+$lang['apr_do_approve']        = 'Odobri';
---
+++ lang/hr/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Davor Turkalj <turki.bsc@gmail.com>
+ */
+$lang['apr_namespaces']        = 'Razmacima odvojeni Namespace-ovi na koje se plugin odnosi';
+$lang['apr_approved_text']     = 'Automatski podrazumijevani komentar odobrenja';
