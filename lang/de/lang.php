afa531056365d9b088414b44a4c1d8391ba94d21
Mark Prins <mprins@users.sf.net>
2014-08-14 13:30:24 +0200

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,27 +1,24 @@
 <?php
 
-// banner
-$lang['apr_recent_draft'] = 'Die <a href="%s">aktuellste Version</a> dieses Dokuments ist ein Entwurf.';
-$lang['apr_outdated'] = 'Diese Version wurde durch eine <a href="%s">neuere bestätigte Version</a> <em>ersetzt</em>.';
-$lang['apr_draft'] = 'Diese Version (%s) ist ein <em>Entwurf</em>.';
-$lang['apr_approved'] = 'Diese Version (%s) wurde <em>bestätigt</em> durch %s.';
-$lang['apr_previous'] = 'Die <a href="%s">zuvor bestätigte Version</a> (%s) ist verfügbar.';
-
-// Pending Approvals List
-$lang['apr_p_none'] = 'Es gibt keine Dokumente die bestätigt werden müssen';
-$lang['apr_p_hdr_page'] = 'Seite';
-$lang['apr_p_hdr_previous'] = 'Zuletzt bestätigt';
-$lang['apr_p_hdr_updated'] = 'Aktualisiert';
-$lang['apr_p_approved'] = 'Von %s am <a href="%s">%s</a>';
-
-// Submit (Next to the tick-box to approve a page)
-$lang['apr_do_approve'] = 'Bestätigen';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ */
+$lang['apr_recent_draft']      = 'Die <a href="%s">aktuellste Version</a> dieses Dokuments ist ein Entwurf.';
+$lang['apr_outdated']          = 'Diese Version wurde durch eine <a href="%s">neuere bestätigte Version</a> <em>ersetzt</em>.';
+$lang['apr_draft']             = 'Diese Version (%s) ist ein <em>Entwurf</em>.';
+$lang['apr_approved']          = 'Diese Version (%s) wurde <em>bestätigt</em> durch %s.';
+$lang['apr_previous']          = 'Die <a href="%s">zuvor bestätigte Version</a> (%s) ist verfügbar.';
+$lang['apr_p_none']            = 'Es gibt keine Dokumente die bestätigt werden müssen';
+$lang['apr_p_hdr_page']        = 'Seite';
+$lang['apr_p_hdr_previous']    = 'Zuletzt bestätigt';
+$lang['apr_p_hdr_updated']     = 'Aktualisiert';
+$lang['apr_p_approved']        = 'Von %s am <a href="%s">%s</a>';
+$lang['apr_do_approve']        = 'Bestätigen';
 $lang['wrong permissions to approve'] = 'Sie haben nicht die nötige Berechtigung um diese Seite zu bestätigen.';
 $lang['cannot approve a non-existing revision'] = 'Sie können keine Revision bestätigen die nicht existiert.';
-$lang['version approved'] = 'Sie haben das Dokument überprüft.';
-$lang['cannot approve error'] = 'Überprüfung konnte aus technischen Gründen nicht gesetzt werden. (Berechtigungsfehler?)';
-
-$lang['approvals'] = 'Überprüfungen: %s/%s';
-$lang['approved by'] = 'Überprüft durch: %s';
-
-$lang['approve action'] = 'Dokument bestätigen';
+$lang['version approved']      = 'Sie haben das Dokument überprüft.';
+$lang['cannot approve error']  = 'Überprüfung konnte aus technischen Gründen nicht gesetzt werden. (Berechtigungsfehler?)';
+$lang['approvals']             = 'Überprüfungen: %s/%s';
+$lang['approved by']           = 'Überprüft durch: %s';
+$lang['approve action']        = 'Dokument bestätigen';
