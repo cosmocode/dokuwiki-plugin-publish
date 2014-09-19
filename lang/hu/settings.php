91200cf0b74e2ef7c53c48ff4385d47bf5d30739
DelDadam <deldadam@gmail.com>
2014-09-19 20:55:05 +0200

    translation update

--- lang/hu/settings.php
+++ lang/hu/settings.php
@@ -4,6 +4,13 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Marina Vladi <deldadam@gmail.com>
+ * @author DelDadam <deldadam@gmail.com>
  */
-$lang['apr_namespaces']        = 'Névterek, amelyekben a bővítmény aktív';
-$lang['apr_approved_text']     = 'Automatikus jóváhagyás megjegyzése';
+$lang['apr_namespaces']        = 'Névterek, amelyekben a bővítmény aktív (szóközzel elválasztott lista).';
+$lang['no_apr_namespaces']     = 'Névterek, amelyekben a bővítmény <strong>nem</strong> aktív (szóközzel elválasztott lista).';
+$lang['number_of_approved']    = 'Oldal jóváhagyásához szükséges felhasználók száma.';
+$lang['hidereaderbanner']      = 'Szalagcím elrejtése csak olvasási joggal rendelkező felhasználók elől.';
+$lang['hide drafts']           = 'Piszkozatok elrejtése csak olvasási joggal rendelkező felhasználók elől.';
+$lang['author groups']         = 'Piszkozatok megtekintésére jogosult felhasználói csoportok (szóközzel elválasztott lista).';
+$lang['internal note']         = 'Nem jóváhagyott oldalakon megjelenő megjegyzés.';
+$lang['delete attic on first approve'] = 'Az <i>attic</i> könyvtárban tárolt változat törlése az első jóváhagyáskor.';
