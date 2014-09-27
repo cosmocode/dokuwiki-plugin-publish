79c708ac2ebb1f5082af9dc3aa82e56ee032e764
Christopher Schive <chschive@frisurf.no>
2014-09-28 01:20:05 +0200

    translation update

--- lang/no/lang.php
+++ lang/no/lang.php
@@ -1,20 +1,25 @@
 <?php
 
-// banner
-$lang['apr_recent_draft'] = 'Den <a href="%s">nyeste versjonen</a> av dette dokumentet er et utkast.';
-$lang['apr_outdated'] = 'Denne versjonen er <em>utdatert</em> av en <a href="%s">nyere godkjent versjon</a>.';
-$lang['apr_draft'] = 'Denne versjonen (%s) er et <em>utkast</em>.';
-$lang['apr_approved'] = 'Denne versjonen (%s) ble <em>godkjent</em> av %s.';
-$lang['apr_previous'] = 'Den <a href="%s">tidligere godkjente versjonen</a> (%s) er tilgjengelig.';
-
-// liste med ventende godkjenninger
-$lang['apr_p_none'] = 'Ingen dokumenter venter på godkjenning';
-$lang['apr_p_hdr_page'] = 'Side';
-$lang['apr_p_hdr_previous'] = 'Tidligere godkjenning';
-$lang['apr_p_hdr_updated'] = 'Oppdatert';
-$lang['apr_p_approved'] = 'Av %s den <a href="%s">%s</a>';
-
-// ledetekst (Ved siden av boksen for godkjenning av en side)
-$lang['apr_do_approve'] = 'Godkjenn';
-
-?>
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Christopher Schive <chschive@frisurf.no>
+ */
+$lang['apr_recent_draft']      = 'Den <a href="%s">nyeste versjonen</a> av dette dokumentet er et utkast.';
+$lang['apr_outdated']          = 'Denne versjonen er <em>utdatert</em> av en <a href="%s">nyere godkjent versjon</a>.';
+$lang['apr_draft']             = 'Denne versjonen (%s) er et <em>utkast</em>.';
+$lang['apr_approved']          = 'Denne versjonen (%s) ble <em>godkjent</em> av %s.';
+$lang['apr_previous']          = 'Den <a href="%s">tidligere godkjente versjonen</a> (%s) er tilgjengelig.';
+$lang['apr_p_none']            = 'Ingen dokumenter venter på godkjenning';
+$lang['apr_p_hdr_page']        = 'Side';
+$lang['apr_p_hdr_previous']    = 'Tidligere godkjenning';
+$lang['apr_p_hdr_updated']     = 'Oppdatert';
+$lang['apr_p_approved']        = 'Av %s den <a href="%s">%s</a>';
+$lang['apr_do_approve']        = 'Godkjenn';
+$lang['wrong permissions to approve'] = 'Du har ikke rettigheter til å godkjenne denne siden.';
+$lang['cannot approve a non-existing revision'] = 'Du kan ikke godkjenne en ikke-eksisterende revisjon.';
+$lang['version approved']      = 'Du har godkjent dokumentet.';
+$lang['cannot approve error']  = 'Kan ikke godkjenne pga. tekniske feil (rettigheter?)';
+$lang['approvals']             = 'Godkjenninger: %s/%s';
+$lang['approved by']           = 'Godkjent av: %s';
+$lang['approve action']        = 'Godkjenn dokument';
--- lang/no/settings.php
+++ lang/no/settings.php
@@ -1,6 +1,15 @@
 <?php
 
-$lang['apr_namespaces'] = 'Navnerom som dette tillegget gjelder for';
-$lang['apr_approved_text'] = 'Automatisk godkjenningskommentar';
-
-?>
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Christopher Schive <chschive@frisurf.no>
+ */
+$lang['apr_namespaces']        = 'Navnerom som dette tillegget gjelder for';
+$lang['no_apr_namespaces']     = 'Navnerom som plugin-en <strong>ikke skal</strong> gjelde for (liste atskilt med mellomrom).';
+$lang['number_of_approved']    = 'Antall brukere som trengs for å godkjenne en side.';
+$lang['hidereaderbanner']      = 'Skjul banner for brukere som kun har leserettigheter';
+$lang['hide drafts']           = 'Skjul utkast for brukere som kun har leserettigheter';
+$lang['author groups']         = 'Grupper som kan se utkast (atskilt med mellomrom)';
+$lang['internal note']         = 'Tekst på ikke-godkjente sider';
+$lang['delete attic on first approve'] = 'Slett tidligere revisjoner ved godkjenning';
