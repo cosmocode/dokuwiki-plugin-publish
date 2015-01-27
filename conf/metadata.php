269d75574bbb6938447db274e2f5e5326a4e4af6
Michael Große <grosse@cosmocode.de>
2015-01-27 12:18:29 +0100

    Add option to hide the banner, if the page is approved

--- conf/metadata.php
+++ conf/metadata.php
@@ -5,6 +5,7 @@ $meta['no_apr_namespaces'] = array('string');
 $meta['number_of_approved'] = array('numeric', '_min' => 1);
 $meta['hide drafts'] = array('onoff');
 $meta['hidereaderbanner'] = array('onoff');
+$meta['hide approved banner'] = array('onoff');
 $meta['author groups'] = array('string');
 $meta['internal note'] = array('string');
 $meta['delete attic on first approve'] = array('onoff');
--- lang/de/settings.php
+++ lang/de/settings.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Mark Prins <mprins@users.sf.net>
  */
 $lang['apr_namespaces']        = 'Namensräume auf die dieses Plugin angewendet werden soll';
@@ -10,6 +10,7 @@ $lang['no_apr_namespaces']     = 'Namensräume, auf die dieses Plugin <strong>ni
 $lang['number_of_approved']    = 'Anzahl der Benutzer die das Dokument prüfen müssen, damit es als bestätigt gilt.';
 $lang['hidereaderbanner']      = 'Verstecke Banner für Benutzern die nur das Leserecht haben';
 $lang['hide drafts']           = 'Verstecke unbestätigte Versionen vor Benutzern die nur das Leserecht haben.';
+$lang['hide approved banner']  = 'Verstecke Banner auf bestätigten Seiten.';
 $lang['author groups']         = 'Gruppen die drafts sehen können (mehrere mit Leerzeichen trennen)';
 $lang['internal note']         = 'Notiz auf nicht veröffentlichen Seiten';
 $lang['delete attic on first approve'] = 'Alter Versionen bei erster Bestätigung löschen';
