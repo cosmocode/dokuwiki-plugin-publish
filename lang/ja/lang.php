5d01ac5aa20c422a16c7f576072074d93bd5c2b5
Hideaki SAWADA <chuno@live.jp>
2015-07-10 12:20:07 +0200

    translation update

---
+++ lang/ja/lang.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['apr_recent_draft']      = 'この文章の<a href="%s">最新バージョン</a>は草案です。';
+$lang['apr_outdated']          = 'このバージョンは<a href="%s">新しい承認済バージョン</a>より<em>古い</em>です。';
+$lang['apr_draft']             = 'このバージョン（%s）は<em>草案</em>です。';
+$lang['apr_approved']          = 'このバージョン（%s）は<em>承認済</em>（承認者：%s）です。';
+$lang['apr_previous']          = '<a href="%s">前の承認済バージョン</a>（%s）が利用可能です。';
+$lang['apr_p_none']            = '承認保留中の文章がありません';
+$lang['apr_p_hdr_page']        = 'ページ';
+$lang['apr_p_hdr_previous']    = '前の承認';
+$lang['apr_p_hdr_updated']     = 'Updated';
+$lang['apr_p_approved']        = 'By %s on <a href="%s">%s</a>';
+$lang['apr_do_approve']        = '承認する';
+$lang['wrong permissions to approve'] = 'このページを承認する権限がありません。';
+$lang['cannot approve a non-existing revision'] = '存在しないリビジョンは承認できません。';
+$lang['version approved']      = 'この文章は承認済です。';
+$lang['cannot approve error']  = '技術的なエラー（権限とか？）によって承認できませんでした。';
+$lang['approvals']             = '承認：%s/%s';
+$lang['approved by']           = '承認者：%s';
+$lang['approve action']        = '文章の承認';
+$lang['mail_invalid']          = '公開プラグインから変更通知用に登録したメールアドレスが無効です：%s';
+$lang['apr_mail_subject']      = '新規草案';
+$lang['apr_mail_app_subject']  = '草案が承認されました';
