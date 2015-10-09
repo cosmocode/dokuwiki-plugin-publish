1367b5ef0807a64fb92859ca341f785d080267ae
Carsten Perthel <carsten@cpesoft.com>
2015-10-09 15:29:57 +0200

    Fix some typos and commas

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -5,14 +5,14 @@
  *
  * @author Mark Prins <mprins@users.sf.net>
  */
-$lang['apr_namespaces']        = 'Namensräume auf die dieses Plugin angewendet werden soll';
+$lang['apr_namespaces']        = 'Namensräume, auf die dieses Plugin angewendet werden soll';
 $lang['no_apr_namespaces']     = 'Namensräume, auf die dieses Plugin <strong>nicht</strong> angewendet werden soll';
-$lang['number_of_approved']    = 'Anzahl der Benutzer die das Dokument prüfen müssen, damit es als bestätigt gilt.';
-$lang['hidereaderbanner']      = 'Verstecke Banner für Benutzern die nur das Leserecht haben';
-$lang['hide drafts']           = 'Verstecke unbestätigte Versionen vor Benutzern die nur das Leserecht haben.';
+$lang['number_of_approved']    = 'Anzahl der Benutzer, die das Dokument prüfen müssen, damit es als bestätigt gilt.';
+$lang['hidereaderbanner']      = 'Verstecke Banner vor Benutzern, die nur das Leserecht haben';
+$lang['hide drafts']           = 'Verstecke unbestätigte Versionen vor Benutzern, die nur das Leserecht haben.';
 $lang['hide_approved_banner']  = 'Verstecke Banner auf bestätigten Seiten.';
-$lang['author groups']         = 'Gruppen die drafts sehen können (mehrere mit Leerzeichen trennen)';
-$lang['internal note']         = 'Notiz auf nicht veröffentlichen Seiten';
-$lang['delete attic on first approve'] = 'Alter Versionen bei erster Bestätigung löschen';
+$lang['author groups']         = 'Gruppen, die Drafts sehen können (mehrere mit Leerzeichen trennen)';
+$lang['internal note']         = 'Notiz auf nicht veröffentlichten Seiten';
+$lang['delete attic on first approve'] = 'Alte Versionen bei erster Bestätigung löschen';
 
 
