1a2ac9cfd0bdf692cf66e155bfce73a0c02ae421
Carsten Perthel <carsten@cpesoft.com>
2015-10-09 15:57:52 +0200

    translation update (de)

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -4,6 +4,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
  * @author Mark Prins <mprins@users.sf.net>
+ * @author Carsten Perthel <carsten@cpesoft.com>
  */
 $lang['apr_namespaces']        = 'Namensräume, auf die dieses Plugin angewendet werden soll';
 $lang['no_apr_namespaces']     = 'Namensräume, auf die dieses Plugin <strong>nicht</strong> angewendet werden soll';
@@ -15,4 +16,6 @@ $lang['author groups']         = 'Gruppen, die Drafts sehen können (mehrere mit
 $lang['internal note']         = 'Notiz auf nicht veröffentlichten Seiten';
 $lang['delete attic on first approve'] = 'Alte Versionen bei erster Bestätigung löschen';
 
-
+$lang['send_mail_on_approve'] = 'Email senden, wenn eine Seite bestätigt wurde?';
+$lang['apr_mail_receiver'] = 'Standard Mailadresse für Änderungen. Wenn eingetragen, wird eine Email nach jeder Seitenänderung verschickt.';
+$lang['apr_approved_text'] = 'Standard Bestätigungs-Marker';
