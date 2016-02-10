e916fcf61a5982b1c036fc34709c3041e48ddde6
Dana <dannax3@gmx.de>
2016-02-10 15:15:07 +0100

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- *
+ * 
  * @author Mark Prins <mprins@users.sf.net>
  * @author Carsten Perthel <carsten@cpesoft.com>
  */
@@ -15,7 +15,6 @@ $lang['hide_approved_banner']  = 'Verstecke Banner auf bestätigten Seiten.';
 $lang['author groups']         = 'Gruppen, die Drafts sehen können (mehrere mit Leerzeichen trennen)';
 $lang['internal note']         = 'Notiz auf nicht veröffentlichten Seiten';
 $lang['delete attic on first approve'] = 'Alte Versionen bei erster Bestätigung löschen';
-
-$lang['send_mail_on_approve'] = 'Email senden, wenn eine Seite bestätigt wurde?';
-$lang['apr_mail_receiver'] = 'Standard Mailadresse für Änderungen. Wenn eingetragen, wird eine Email nach jeder Seitenänderung verschickt.';
-$lang['apr_approved_text'] = 'Standard Bestätigungs-Marker';
+$lang['send_mail_on_approve']  = 'Email senden, wenn eine Seite bestätigt wurde?';
+$lang['apr_mail_receiver']     = 'Standard Mailadresse für Änderungen. Wenn eingetragen, wird eine Email nach jeder Seitenänderung verschickt.';
+$lang['apr_approved_text']     = 'Standard Bestätigungs-Marker';
