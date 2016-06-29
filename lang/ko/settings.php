b51730ab888cbec800b541d0728c8486bfa6dbf5
Myeongjin <aranet100@gmail.com>
2016-06-29 19:40:08 +0200

    translation update

--- lang/ko/settings.php
+++ lang/ko/settings.php
@@ -2,8 +2,18 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Myeongjin <aranet100@gmail.com>
  */
-$lang['apr_namespaces']        = '이 플러그인을 적용할 이름공간';
-$lang['apr_approved_text']     = '자동적인 승인 덧글';
+$lang['apr_namespaces']        = '이 플러그인을 적용할 이름공간 (스페이스로 구분된 목록).';
+$lang['no_apr_namespaces']     = '이 플러그인을 적용하지 <strong>않을</strong> 이름공간 (스페이스로 구분된 목록).';
+$lang['number_of_approved']    = '문서 승인에 필요한 사용자 수.';
+$lang['hidereaderbanner']      = '읽기 전용 사용자에게 배너를 숨기기';
+$lang['hide drafts']           = '읽기 전용 사용자에게 초안을 숨기기';
+$lang['hide_approved_banner']  = '승인된 문서에서 배너를 숨기기';
+$lang['author groups']         = '초안을 볼 수 있는 그룹 (빈 칸으로 구분)';
+$lang['internal note']         = '승인되지 않은 문서에서의 참고';
+$lang['delete attic on first approve'] = '첫 승인에서의 다락방 삭제';
+$lang['send_mail_on_approve']  = '문서가 승인되었을 때 이메일을 보낼까요?';
+$lang['apr_mail_receiver']     = '기본 바뀜 이메일 받는이. 문서가 바뀌었을 때 이메일을 보내려면 이것을 사용하세요';
+$lang['apr_approved_text']     = '기본 승인된 마커';
