d5375214377d99eb42ef74ca53e997c65741b398
klapinklapin <klapinklapin@gmail.com>
2016-10-11 23:50:22 +0200

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -2,16 +2,17 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Rene <wllywlnt@yahoo.com>
  * @author Mark Prins <mprins@users.sf.net>
  * @author Peter van Diest <peter.van.diest@xs4all.nl>
+ * @author klapinklapin <klapinklapin@gmail.com>
  */
-$lang['apr_recent_draft']      = 'De <a href="%s">Most Recent Version</a> van dit document is een concept.';
-$lang['apr_outdated']          = 'Deze versie is <em>Outdated</em> door een <a href="%s">Newer Approved Version</a>.';
-$lang['apr_draft']             = 'Deze versie (%s) is een <em>Draft</em>.';
-$lang['apr_approved']          = 'Deze versie (%s) is <em>Approved</em> door %s.';
-$lang['apr_previous']          = 'De <a href="%s">Previously Approved Version</a> (%s) is beschikbaar.';
+$lang['apr_recent_draft']      = 'De <a href="%s">Meest Recente Versie</a> van dit document is een concept.';
+$lang['apr_outdated']          = 'Deze versie is <em>Verouderd</em> door een <a href="%s">Nieuwere Goedgekeurde Versie</a>.';
+$lang['apr_draft']             = 'Deze versie (%s) is een <em>Concept</em>.';
+$lang['apr_approved']          = 'Deze versie (%s) is <em>Goedgekeurd</em> door %s.';
+$lang['apr_previous']          = 'De <a href="%s">Eerder goedgekeurde versie</a> (%s) is beschikbaar.';
 $lang['apr_p_none']            = 'Geen documenten ter goedkeuring';
 $lang['apr_p_hdr_page']        = 'Pagina';
 $lang['apr_p_hdr_previous']    = 'Vorige goedkeuring';
@@ -28,3 +29,4 @@ $lang['approve action']        = 'Document goedkeuren';
 $lang['mail_invalid']          = 'Eén van de e-mailadressen voor wijzigingsnotificatie vanuit de Publish-plugin is ongeldig: %s';
 $lang['apr_mail_subject']      = 'Nieuwe suggestie';
 $lang['apr_mail_app_subject']  = 'Suggestie goedgekeurd';
+$lang['approveNS']             = 'Namespace goedkeuren';
