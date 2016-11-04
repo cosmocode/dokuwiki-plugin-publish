aa6204b34ec315e7ad7f37a47c094f4b554b22b3
flallo75 <flaviocolnago@gmail.com>
2016-11-04 16:27:10 +0100

    create lang.php
    support for italian language

---
+++ lang/it/lang.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ */
+
+// banner
+$lang['apr_recent_draft'] = 'La <a href="%s"> versione pi&ugrave recente</a> di questa pagina &egrave una bozza.';
+$lang['apr_outdated'] = 'Questa versione &egrave <em>superata</em> da una <a href="%s">nuova versione approvata</a>.';
+$lang['apr_draft'] = 'Questa versione (%s) &egrave una <em>bozza</em>.';
+$lang['apr_approved'] = 'Questa versione (%s) &egrave stata <em>approvata</em> da %s.';
+$lang['apr_previous'] = 'La <a href="%s">Precedente versione approvata</a> (%s) &egrave disponibile.';
+
+// Pending Approvals List
+$lang['apr_p_none'] = 'Nessun documento in attesa di approvazione';
+$lang['apr_p_hdr_page'] = 'Pagina';
+$lang['apr_p_hdr_previous'] = 'Approvazione precedente';
+$lang['apr_p_hdr_updated'] = 'Superata';
+$lang['apr_p_approved'] = 'da %s su <a href="%s">%s</a>';
+
+// Submit (Next to the tick-box to approve a page)
+$lang['apr_do_approve'] = 'Approva';
+
+
+$lang['wrong permissions to approve'] = 'Non hai diritti sufficienti per approvare questa pagina.';
+$lang['cannot approve a non-existing revision'] = 'Non puoi approvare una revisione che non esiste.';
+$lang['version approved'] = 'Hai approvato il documento.';
+$lang['cannot approve error'] = 'Non puoi approvare per problemi tecnici (permessi?)';
+
+$lang['approvals'] = 'Approvazioni: %s/%s';
+$lang['approved by'] = 'Approvata da: %s';
+$lang['approve action'] = 'Approva il documento';
+
+$lang['mail_invalid'] = 'Un indirizzo email inserito nel plugin publish per la notifica dei cambiamenti non &egrave valida: %s';
+
+$lang['apr_do_approve'] = 'Approva';
+$lang['apr_mail_subject'] = 'nuovo suggerimento';
+$lang['apr_mail_app_subject'] = 'suggerimento approvato';
+
+$lang['approveNS'] = 'Approva il namespace';
