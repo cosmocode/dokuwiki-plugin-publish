b8f55c76450cf8e74efe16f74ab226510d081e37
per-hed <per-hed@users.noreply.github.com>
2017-01-27 15:28:38 +0100

    Swedish translation

---
+++ lang/sv/lang.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ */
+
+// banner
+$lang['apr_recent_draft'] = 'Den <a href="%s">senaste versionen</a> av den här sidan är ett utkast.';
+$lang['apr_outdated'] = 'Den här versionen är <em>ersatt</em> av en <a href="%s">nyare godkänd version</a>.';
+$lang['apr_draft'] = 'Denna version (%s) är ett <em>utkast</em>.';
+$lang['apr_approved'] = 'Denna version (%s) har <em>godkänts</em> av %s.';
+$lang['apr_previous'] = 'Den <a href="%s">tidigare godkända versionen</a> (%s) är tillgänglig.';
+
+// Pending Approvals List
+$lang['apr_p_none'] = 'Inga dokument väntar på godkännande';
+$lang['apr_p_hdr_page'] = 'Sida';
+$lang['apr_p_hdr_previous'] = 'Föregående godkännande';
+$lang['apr_p_hdr_updated'] = 'Uppdaterad';
+$lang['apr_p_approved'] = 'av %s på <a href="%s">%s</a>';
+
+// Submit (Next to the tick-box to approve a page)
+$lang['apr_do_approve'] = 'Godkänn';
+
+
+$lang['wrong permissions to approve'] = 'Du saknar behörighet att godkänna denna sida.';
+$lang['cannot approve a non-existing revision'] = 'Du kan inte godkänna en icke-existerande ändring.';
+$lang['version approved'] = 'Du har godkänt dokumentet.';
+$lang['cannot approve error'] = 'Kan ej godkänna på grund av tekniska fel (behörigheter?)';
+
+$lang['approvals'] = 'Godkännanden: %s/%s';
+$lang['approved by'] = 'Godkänd av: %s';
+$lang['approve action'] = 'Godkänn dokument';
+
+$lang['mail_invalid'] = 'En av e-postadresserna för ändringsnotifieringar från publishing-modulen är ogiltig: %s';
+
+$lang['apr_do_approve'] = 'Godkänn';
+$lang['apr_mail_subject'] = 'nytt förslag';
+$lang['apr_mail_app_subject'] = 'förslag godkänt';
+
+$lang['approveNS'] = 'Godkänn namnrymd';
