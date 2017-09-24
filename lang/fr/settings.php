cbb4cef39ced5bfd3e503b8ecb5921234a78192e
Schplurtz le Déboulonné <schplurtz@laposte.net>
2017-09-24 13:15:31 +0200

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -7,10 +7,11 @@
  * @author Nicolas Friedli <nicolas@theologique.ch>
  * @author Grégory Gutierez <gregory.gutierez@gmail.com>
  * @author lerdt <pro@nicolas-hemard.eu>
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  */
-$lang['apr_namespaces']        = 'Namespaces -> Espaces de noms
-this plugin applies to -> Ce plugin s\'applique à';
-$lang['no_apr_namespaces']     = 'Espaces de noms auxquels ce plugin <strong>ne s\'applique pas</strong> (liste séparée par des epsaces)';
+$lang['apr_namespaces']        = 'Catégories auxquelles ce greffon s\'applique (liste séparée par des espaces).
+';
+$lang['no_apr_namespaces']     = 'Catégories auxquelles ce greffon <strong>ne s\'applique pas</strong> (liste séparée par des espaces).';
 $lang['number_of_approved']    = 'Nombre d\'utilisateurs requis pour approver une page.';
 $lang['hidereaderbanner']      = 'Cacher la bannière aux utilisateurs en lecture seule';
 $lang['hide drafts']           = 'Cacher les brouillons aux utilisateurs en lecture seule';
@@ -19,3 +20,5 @@ $lang['author groups']         = 'Groupes ayant le droit de voir les brouillons 
 $lang['internal note']         = 'Note sur les pages non approuvées';
 $lang['delete attic on first approve'] = 'Effacer la sauvegarde à la première approbation';
 $lang['send_mail_on_approve']  = 'Envoyer un mail lorsque cette page aura été approuvée ?';
+$lang['apr_mail_receiver']     = 'Destinataire des notifications de changement. Indiquez l\'adresse qui recevra un courriel lorsqu\'une page change.';
+$lang['apr_approved_text']     = 'marqueur d\'approbation par défaut.';
