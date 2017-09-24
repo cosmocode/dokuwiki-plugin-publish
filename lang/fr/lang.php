e924299e0392a66df74a0cbe8b0873058f8e8c9d
Schplurtz le Déboulonné <Schplurtz@laposte.net>
2017-09-24 23:55:31 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -9,17 +9,17 @@
  * @author lerdt <pro@nicolas-hemard.eu>
  * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  */
-$lang['apr_recent_draft']      = 'La <a href="%s">version la plus récente</a> de ce document est un projet.';
+$lang['apr_recent_draft']      = 'La <a href="%s">version la plus récente</a> de ce document est une ébauche.';
 $lang['apr_outdated']          = 'Cette version est <em>dépassée</ em>. Une <a href="%s">version approuvée plus récente</a> existe.';
-$lang['apr_draft']             = 'Cette version (%s) est en <em>projet</em>.';
+$lang['apr_draft']             = 'Cette version (%s) est en <em>ébauche</em>.';
 $lang['apr_approved']          = '%2$s a <em>approuvé</em> cette version (%1$s).';
 $lang['apr_previous']          = 'La <a href="%s">version précédemment approuvée</a> (%s) est disponible.';
-$lang['apr_p_none']            = 'Pas de documents en attente d\'approbation';
+$lang['apr_p_none']            = 'Pas de documents en attente d\'approbation.';
 $lang['apr_p_hdr_page']        = 'Page';
 $lang['apr_p_hdr_previous']    = 'Approbation précédente';
 $lang['apr_p_hdr_updated']     = 'Mise à jour';
 $lang['apr_p_approved']        = 'Par %s sur <a href="%s">%s</a>';
-$lang['apr_do_approve']        = 'Accepter';
+$lang['apr_do_approve']        = 'Approuver';
 $lang['wrong permissions to approve'] = 'Vous n\'avez pas les permissions nécessaires pour approuver cette page.';
 $lang['cannot approve a non-existing revision'] = 'Vous ne pouvez approuver une révision qui n\'existe pas.';
 $lang['version approved']      = 'Vous avez approuvé le document.';
@@ -27,7 +27,7 @@ $lang['cannot approve error']  = 'Impossible d\'approuver pour des raisons techn
 $lang['approvals']             = 'Approuvés: %s/%s';
 $lang['approved by']           = 'Approuvés par: %s';
 $lang['approve action']        = 'Approuver le document';
-$lang['mail_invalid']          = 'Une des adresses e-mail saisies pour notifier les changements est invalide (greffon Publish) : %s';
+$lang['mail_invalid']          = 'L\'une des adresses e-mail saisies pour que le greffon Publish notifie les changements est invalide : %s';
 $lang['apr_mail_subject']      = 'Nouvelle suggestion';
 $lang['apr_mail_app_subject']  = 'Suggestion approuvée';
 $lang['approveNS']             = 'Approuver cette catégorie.';
