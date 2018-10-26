c50b5e3e1ff85d8e5d9d0ca97bd4d617f95001ec
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2018-10-26 22:05:54 +1000

    Update settings.php

--- lang/ru/settings.php
+++ lang/ru/settings.php
@@ -5,12 +5,19 @@
  * 
  * @author Aleksandr Selivanov <alexgearbox@gmail.com>
  * @author Vitaly Filatenko <kot@hacktest.net>
+ * @author Rainbow-Spike <rainbow_spike@derpy.ru>
  */
+
 $lang['apr_namespaces']        = 'Пространства имён, к которым применяется плагин (разделять пробелом).';
 $lang['no_apr_namespaces']     = 'Пространство имён, к которым плагин <strong>не применяется</strong> (разделять пробелом)';
 $lang['number_of_approved']    = 'Необходимое количество пользователей для одобрения';
 $lang['hidereaderbanner']      = 'Прятать баннер для read-only пользователей';
 $lang['hide drafts']           = 'Прятать черновики для read-only пользователей';
+$lang['hide_approved_banner']  = 'Прятать баннер на одобренных страницах';
 $lang['author groups']         = 'Группы, которые могут видеть черновики (разделять пробелом)';
 $lang['internal note']         = 'Примечание на непроверенных страницах';
 $lang['delete attic on first approve'] = 'Удалять историю правок при первом одобрении';
+
+$lang['send_mail_on_approve']  = 'Послать письмо при одобрении правки?';
+$lang['apr_mail_receiver']     = 'Получатель писем по умолчанию. Проставьте адрес для писем об изменениях страниц';
+$lang['apr_approved_text']     = 'Метка одобрения по умолчанию';
