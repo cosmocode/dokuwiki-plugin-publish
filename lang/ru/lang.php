3f353a310d306747506a35df47bdbf482a634473
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2019-01-11 01:20:56 +1000

    Russian translation update (#127)

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -5,22 +5,40 @@
  * 
  * @author Aleksandr Selivanov <alexgearbox@gmail.com>
  * @author Vitaly Filatenko <kot@hacktest.net>
+ * @author Rainbow-Spike <rainbow_spike@derpy.ru>
  */
+
+// banner
 $lang['apr_recent_draft']      = '<a href="%s">Последняя версия</a> этой страницы является черновой.';
 $lang['apr_outdated']          = 'Эта версия <em>устарела</em>, есть <a href="%s">новая проверенная версия</a>.';
 $lang['apr_draft']             = 'Это <em>черновая</em> версия (%s).';
 $lang['apr_approved']          = 'Эта версия (%s) была <em>проверена</em> пользователем <i>%s</i>.';
 $lang['apr_previous']          = 'Доступна <a href="%s">предыдущая проверенная версия</a> (%s).';
+
+// Pending Approvals List
 $lang['apr_p_none']            = 'Нет ожидающих проверки документов.';
 $lang['apr_p_hdr_page']        = 'Страницы';
 $lang['apr_p_hdr_previous']    = 'Предыдущая проверка';
 $lang['apr_p_hdr_updated']     = 'Обновлено';
 $lang['apr_p_approved']        = 'Проверено %s <a href="%s">%s</a>';
+
+// Submit (Next to the tick-box to approve a page)
 $lang['apr_do_approve']        = 'Одобрить';
+
+
 $lang['wrong permissions to approve'] = 'У вас нет прав для одобрения этой страницы.';
 $lang['cannot approve a non-existing revision'] = 'Вы не можете одобрить несуществующую правку.';
 $lang['version approved']      = 'Вы одобрили этот документ.';
 $lang['cannot approve error']  = 'Невозможно одобрить из-за технических проблем (права доступа?)';
+
 $lang['approvals']             = 'Проверили: %s/%s';
 $lang['approved by']           = 'Проверено: %s';
 $lang['approve action']        = 'Одобрить документ';
+
+$lang['mail_invalid'] = 'Один из e-mail адресов для сообщений об изменениях от плагина publish оказался неправильным: %s';
+
+$lang['apr_do_approve'] = 'Одобрить';
+$lang['apr_mail_subject'] = 'новое предложение';
+$lang['apr_mail_app_subject'] = 'предложение одобрено';
+
+$lang['approveNS'] = 'Одобрить папку';
