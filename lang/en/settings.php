3ce767b2b3740da155432e2ef1d1fc47bbfedf03
Viktor <viktor.kondurake@gmail.com>
2019-02-24 07:20:07 +0200

    Add the "approving_weights" description

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -3,6 +3,7 @@
 $lang['apr_namespaces'] = 'Namespaces this plugin applies to (space separated list).';
 $lang['no_apr_namespaces'] = 'Namespaces this plugin <strong>does not</strong> apply to (space separated list).';
 $lang['number_of_approved'] = 'Number of users needed to approve a page.';
+$lang['approving_weights'] = 'Groups and the number of approvals required to approve a page (for example, if <i>number_of_approved=10</i> and this option is defined as <i>admin=10&moderator=5&user=1</i>, then a page can be approved by one admin, two moderators, ten users, or one moderator and five users)';
 $lang['hidereaderbanner'] = 'Hide banner to read only users';
 $lang['hide drafts'] = 'Hide drafts to read only users';
 $lang['hide_approved_banner']  = 'Hide banner on approved pages';
