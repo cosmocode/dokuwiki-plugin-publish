a1e603f337282eee221edb3eef9f7c4ce8e048cf
Bartek S <sadupl@gmail.com>
2019-10-08 19:55:44 +0200

    translation update

--- lang/pl/lang.php
+++ lang/pl/lang.php
@@ -1,18 +1,21 @@
 <?php
 
-// banner
-$lang['apr_recent_draft'] = '<a href="%s">Najnowsza wersja</a> tego dokumentu jest niezatwierdzona.';
-$lang['apr_outdated'] = 'Ta wersja strony została <em>zdezaktualizowana</em> przez <a href="%s">nową zatwierdzoną werjsę</a>.';
-$lang['apr_draft'] = 'Ta wersja strony (%s) jest <em>niezatwierdzona</em>.';
-$lang['apr_approved'] = 'Ta wersja strony(%s) została <em>zatwierdzona</em> przez %s.';
-$lang['apr_previous'] = 'Dostępna <a href="%s">poprzednio zatwierdzona wersja</a> (%s).';
-
-// Pending Approvals List
-$lang['apr_p_none'] = 'Brak dokumentów oczekujących na zatwierdzenie';
-$lang['apr_p_hdr_page'] = 'Nazwa strony';
-$lang['apr_p_hdr_previous'] = 'Ostatnia zatwierdzona wersja';
-$lang['apr_p_hdr_updated'] = 'Data ostatniej zmiany';
-$lang['apr_p_approved'] = 'Zatwierdzona przez %s dnia: <a href="%s">%s</a>';
-
-// Submit (Next to the tick-box to approve a page)
-$lang['apr_do_approve'] = 'Zatwierdź';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Bartek S <sadupl@gmail.com>
+ */
+$lang['apr_recent_draft']      = '<a href="%s">Najnowsza wersja</a> tego dokumentu jest niezatwierdzona.';
+$lang['apr_outdated']          = 'Ta wersja strony została <em>zdezaktualizowana</em> przez <a href="%s">nową zatwierdzoną werjsę</a>.';
+$lang['apr_draft']             = 'Ta wersja strony (%s) jest <em>niezatwierdzona</em>.';
+$lang['apr_approved']          = 'Ta wersja strony(%s) została <em>zatwierdzona</em> przez %s.';
+$lang['apr_previous']          = 'Dostępna <a href="%s">poprzednio zatwierdzona wersja</a> (%s).';
+$lang['apr_p_none']            = 'Brak dokumentów oczekujących na zatwierdzenie';
+$lang['apr_p_hdr_page']        = 'Nazwa strony';
+$lang['apr_p_hdr_previous']    = 'Ostatnia zatwierdzona wersja';
+$lang['apr_p_hdr_updated']     = 'Data ostatniej zmiany';
+$lang['apr_p_approved']        = 'Zatwierdzona przez %s dnia: <a href="%s">%s</a>';
+$lang['apr_do_approve']        = 'Zatwierdź';
+$lang['approved by']           = 'Zatwierdzony przez" %s';
+$lang['apr_mail_subject']      = 'nowa sugestia';
+$lang['apr_mail_app_subject']  = 'sugestia zaakceptowana';
--- lang/pl/settings.php
+++ lang/pl/settings.php
@@ -1,4 +1,12 @@
 <?php
 
-$lang['apr_namespaces'] = 'Preztrzenie nazw (ang. namespaces), które wtyczka ma obejmować (puste pole oznacza całe wiki)';
-$lang['apr_approved_text'] = 'Komentarz do operacji zatwierdzenia strony';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Bartek S <sadupl@gmail.com>
+ */
+$lang['apr_namespaces']        = 'Preztrzenie nazw (ang. namespaces), które wtyczka ma obejmować (puste pole oznacza całe wiki)';
+$lang['number_of_approved']    = 'Liczba użytkowników potrzebna do zatwierdzenia strony.';
+$lang['internal note']         = 'Notatka do nie zatwierdzonej strony';
+$lang['send_mail_on_approve']  = 'Wysłać email kiedy strona zostanie zaakceptowana?';
+$lang['apr_approved_text']     = 'Komentarz do operacji zatwierdzenia strony';
