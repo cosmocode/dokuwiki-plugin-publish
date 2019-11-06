8cf7c551ea085b99c73595434bc3b9d576e1aa4a
what exactly is internet marketing <danialhagelthorn@gmail.com>
2019-11-06 22:10:27 +0100

    translation update

--- lang/no/settings.php
+++ lang/no/settings.php
@@ -2,19 +2,26 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author what exactly is internet marketing <danialhagelthorn@gmail.com>
  * @author Christopher Schive <chschive@frisurf.no>
  * @author Daniel Raknes <rada@jbv.no>
  */
-$lang['apr_namespaces']        = 'Navnerom som dette tillegget gjelder for (liste atskilt med mellomrom).';
-$lang['no_apr_namespaces']     = 'Navnerom som plugin-en <strong>ikke skal</strong> gjelde for (liste atskilt med mellomrom).';
-$lang['number_of_approved']    = 'Antall brukere som trengs for å godkjenne en side.';
-$lang['hidereaderbanner']      = 'Skjul banner for brukere som kun har leserettigheter';
-$lang['hide drafts']           = 'Skjul utkast for brukere som kun har leserettigheter';
-$lang['hide_approved_banner']  = 'Skjul banner på godkjente sider';
-$lang['author groups']         = 'Grupper som kan se utkast (atskilt med mellomrom)';
-$lang['internal note']         = 'Tekst på ikke-godkjente sider';
-$lang['delete attic on first approve'] = 'Slett tidligere revisjoner ved godkjenning';
-$lang['send_mail_on_approve']  = 'Send en e-post når en side har blitt godkjent?';
-$lang['apr_mail_receiver']     = 'Standard endrings-epost mottaker. Det sendes en epost til denne adressen når en side endres  ';
-$lang['apr_approved_text']     = 'Standardtekst for godkjenning';
+$lang['apr_namespaces']        = 'Hagelthorn';
+$lang['no_apr_namespaces']     = 'Hagelthorn';
+$lang['number_of_approved']    = 'Well it\'s not very often I bump into a good website like 
+this, cheers, Angie Carrington from Kent.';
+$lang['hidereaderbanner']      = 'Well it\'s not very often I bump into a good website like this, cheers, Angie 
+Carrington from Kent.';
+$lang['hide drafts']           = 'Well it\'s not very often I bump into a good website like this, cheers, Angie Carrington from Kent.';
+$lang['hide_approved_banner']  = 'Well it\'s not very often I bump into a good website like 
+this, cheers, Angie Carrington from Kent.';
+$lang['author groups']         = 'Walter Prada';
+$lang['internal note']         = 'Well it\'s not very often I bump into a good website like this,
+cheers, Angie Carrington from Kent.';
+$lang['delete attic on first approve'] = 'Well it\'s not very often I bump into a good website 
+like this, cheers, Angie Carrington from Kent.';
+$lang['send_mail_on_approve']  = 'danialhagelthorn@gmail.com';
+$lang['apr_mail_receiver']     = 'danialhagelthorn@gmail.com';
+$lang['apr_approved_text']     = 'Well it\'s not very often I bump into a good website 
+like this, cheers, Angie Carrington from Kent.';
