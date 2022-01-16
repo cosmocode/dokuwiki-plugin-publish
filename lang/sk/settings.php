fb1ac96a728f6e9c2b109b2d680099842b1196d1
Viktor Kristian <vkristian@gmail.com>
2022-01-16 12:05:17 +0100

    added translation to sk language

---
+++ lang/sk/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['apr_namespaces'] = 'Menné priestory, ktorých sa tento plugin týka (zoznam oddelený medzerami).';
+$lang['no_apr_namespaces'] = 'Menné priestory, ktorých sa tento plugin <strong>netýka</strong> (zoznam oddelený medzerami).';
+$lang['number_of_approved'] = 'Počet užívateľov potrebných na schválenie stránky.';
+$lang['hidereaderbanner'] = 'Nezobrazovať banner užívateľom s právani len na čítanie';
+$lang['hide drafts'] = 'Nezobrazovať návrhy stránok užívateľom s právami len na čítanie';
+$lang['hide_approved_banner']  = 'Nezobrazovať banner na schválených stránkach';
+$lang['author groups'] = 'Užívatelia, ktorí môžu vidieť návrhy stránok (zoznam oddelený medzerami)';
+$lang['internal note'] = 'Poznámka zobrazená na nechválenej stránke';
+$lang['delete attic on first approve'] = 'Pri prvom schválení stránky zmazať históriu zmien';
+
+$lang['send_mail_on_approve'] = 'Poslať e-mail ak bola stránka schválená?';
+$lang['apr_mail_receiver'] = 'Predvolený prijímateľ e-mailov o zmene stránky. Nastavte túto hodnotu, ak si želáte dostať informačný e-mail o zmene stránky.';
+$lang['apr_approved_text'] = 'Predvolený text pre akciu schválenia';
