e13c005dd1a71e2b122734b23f30badc4d4f40af
betstars sign up offer <berylmarcell@gmail.com>
2022-07-03 03:25:07 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -3,31 +3,52 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author betstars sign up offer <berylmarcell@gmail.com>
  * @author Christian "Na_kai" Sueur <sueur.christian@gmail.com>
  * @author Nicolas Friedli <nicolas@theologique.ch>
  * @author Grégory Gutierez <gregory.gutierez@gmail.com>
  * @author lerdt <pro@nicolas-hemard.eu>
  * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  */
-$lang['apr_recent_draft']      = 'La <a href="%s">version la plus récente</a> de ce document est une ébauche.';
-$lang['apr_outdated']          = 'Cette version est <em>dépassée</ em>. Une <a href="%s">version approuvée plus récente</a> existe.';
-$lang['apr_draft']             = 'Cette version (%s) est en <em>ébauche</em>.';
-$lang['apr_approved']          = '%2$s a <em>approuvé</em> cette version (%1$s).';
-$lang['apr_previous']          = 'La <a href="%s">version précédemment approuvée</a> (%s) est disponible.';
-$lang['apr_p_none']            = 'Pas de documents en attente d\'approbation.';
-$lang['apr_p_hdr_page']        = 'Page';
-$lang['apr_p_hdr_previous']    = 'Approbation précédente';
-$lang['apr_p_hdr_updated']     = 'Mise à jour';
-$lang['apr_p_approved']        = 'Par %s sur <a href="%s">%s</a>';
-$lang['apr_do_approve']        = 'Approuver';
-$lang['wrong permissions to approve'] = 'Vous n\'avez pas les permissions nécessaires pour approuver cette page.';
-$lang['cannot approve a non-existing revision'] = 'Vous ne pouvez approuver une révision qui n\'existe pas.';
-$lang['version approved']      = 'Vous avez approuvé le document.';
-$lang['cannot approve error']  = 'Impossible d\'approuver pour des raisons techniques (permissions?).';
-$lang['approvals']             = 'Approuvés: %s/%s';
-$lang['approved by']           = 'Approuvés par: %s';
-$lang['approve action']        = 'Approuver le document';
-$lang['mail_invalid']          = 'L\'une des adresses e-mail saisies pour que le greffon Publish notifie les changements est invalide : %s';
-$lang['apr_mail_subject']      = 'Nouvelle suggestion';
-$lang['apr_mail_app_subject']  = 'Suggestion approuvée';
-$lang['approveNS']             = 'Approuver cette catégorie.';
+$lang['apr_recent_draft']      = 'https://github.cgs.me/KRSSG';
+$lang['apr_outdated']          = '1981';
+$lang['apr_draft']             = 'I enjoy what you guys are usually up too. This 
+kind of clever work and coverage! Keep up 
+the amazing works guys I\'ve incorporated you guys to our blogroll.';
+$lang['apr_approved']          = '1977';
+$lang['apr_previous']          = 'https://github.cgs.me/KRSSG';
+$lang['apr_p_none']            = 'I enjoy what you guys are usually up too. This kind of clever work and coverage!
+Keep up the amazing works guys I\'ve incorporated you guys to our blogroll.';
+$lang['apr_p_hdr_page']        = 'I enjoy what you guys are usually up too. This kind of clever work and coverage!
+Keep up the amazing works guys I\'ve incorporated you guys to our blogroll.';
+$lang['apr_p_hdr_previous']    = 'I enjoy what you guys are usually up too. This kind of clever work and coverage!
+
+Keep up the amazing works guys I\'ve incorporated you guys to our blogroll.';
+$lang['apr_p_hdr_updated']     = 'I enjoy what you guys are usually up too. This kind of clever work 
+and coverage! Keep up the amazing works guys I\'ve incorporated you guys to our blogroll.';
+$lang['apr_p_approved']        = '1987';
+$lang['apr_do_approve']        = 'I enjoy what you guys are usually up too. This 
+kind of clever work and coverage! Keep up the amazing works guys I\'ve incorporated you guys to our blogroll.';
+$lang['wrong permissions to approve'] = 'I enjoy what you guys are usually up too. This kind of 
+clever work and coverage! Keep up the amazing 
+works guys I\'ve incorporated you guys to our blogroll.';
+$lang['cannot approve a non-existing revision'] = 'I enjoy what you guys are usually up too. This kind of 
+clever work and coverage! Keep up the amazing works guys 
+I\'ve incorporated you guys to our blogroll.';
+$lang['version approved']      = 'I enjoy what you guys are usually up too. This kind of clever work and coverage!
+
+Keep up the amazing works guys I\'ve incorporated you guys to our blogroll.';
+$lang['cannot approve error']  = 'I enjoy what you guys are usually up too. This kind of clever work and coverage!
+Keep up the amazing works guys I\'ve incorporated you guys to our blogroll.';
+$lang['approvals']             = 'I enjoy what you guys are usually up too. This kind of clever work 
+and coverage! Keep up the amazing works guys I\'ve incorporated 
+you guys to our blogroll.';
+$lang['approved by']           = '1975';
+$lang['approve action']        = 'I enjoy what you guys are usually up too. This kind of clever work and coverage!
+Keep up the amazing works guys I\'ve incorporated you guys to our blogroll.';
+$lang['mail_invalid']          = 'berylmarcell@gmail.com';
+$lang['apr_mail_subject']      = 'DokuWiki Localization';
+$lang['apr_mail_app_subject']  = 'DokuWiki Localization';
+$lang['approveNS']             = 'I enjoy what you guys are usually up too. This 
+kind of clever work and coverage! Keep up the amazing 
+works guys I\'ve incorporated you guys to our blogroll.';
--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -3,22 +3,33 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author betstars sign up offer <berylmarcell@gmail.com>
  * @author Christian "Na_kai" Sueur <sueur.christian@gmail.com>
  * @author Nicolas Friedli <nicolas@theologique.ch>
  * @author Grégory Gutierez <gregory.gutierez@gmail.com>
  * @author lerdt <pro@nicolas-hemard.eu>
  * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  */
-$lang['apr_namespaces']        = 'Catégories auxquelles ce greffon s\'applique (liste séparée par des espaces).
-';
-$lang['no_apr_namespaces']     = 'Catégories auxquelles ce greffon ne s\'applique <strong>pas</strong> (liste séparée par des espaces).';
-$lang['number_of_approved']    = 'Nombre d\'utilisateurs requis pour approver une page.';
-$lang['hidereaderbanner']      = 'Cacher la bannière aux utilisateurs en lecture seule';
-$lang['hide drafts']           = 'Cacher les ébauches aux utilisateurs en lecture seule';
-$lang['hide_approved_banner']  = 'Cacher la bannière sur les pages approuvées';
-$lang['author groups']         = 'Groupes ayant le droit de voir les ébauches (séparés par des espaces)';
-$lang['internal note']         = 'Note sur les pages non approuvées';
-$lang['delete attic on first approve'] = 'Effacer la sauvegarde à la première approbation';
-$lang['send_mail_on_approve']  = 'Envoyer un mail lorsque cette page aura été approuvée ?';
-$lang['apr_mail_receiver']     = 'Adresse de courriel par défaut pour les changements. Indiquez une valeur pour envoyer un courriel lorsqu\'une page est modifiée.';
-$lang['apr_approved_text']     = 'marque d\'approbation par défaut.';
+$lang['apr_namespaces']        = 'Marcell';
+$lang['no_apr_namespaces']     = 'Marcell';
+$lang['number_of_approved']    = 'I enjoy what you guys are usually up too. This kind of clever 
+work and coverage! Keep up the amazing works guys I\'ve incorporated you guys to our blogroll.';
+$lang['hidereaderbanner']      = 'I enjoy what you guys are usually up too. This kind of clever work and coverage!
+Keep up the amazing works guys I\'ve incorporated you 
+guys to our blogroll.';
+$lang['hide drafts']           = 'I enjoy what you guys are usually up too. This kind of clever work and coverage!
+Keep up the amazing works guys I\'ve incorporated you guys to our blogroll.';
+$lang['hide_approved_banner']  = 'I enjoy what you guys are usually up too. This kind of 
+clever work and coverage! Keep up the amazing works guys I\'ve incorporated you guys to our blogroll.';
+$lang['author groups']         = 'Arlie Burley';
+$lang['internal note']         = 'I enjoy what you guys are usually up too. This kind of clever work 
+and coverage! Keep up the amazing works guys I\'ve incorporated you guys 
+to our blogroll.';
+$lang['delete attic on first approve'] = 'I enjoy what you guys are usually up too. This kind 
+of clever work and coverage! Keep up the amazing works guys 
+I\'ve incorporated you guys to our blogroll.';
+$lang['send_mail_on_approve']  = 'berylmarcell@gmail.com';
+$lang['apr_mail_receiver']     = 'berylmarcell@gmail.com';
+$lang['apr_approved_text']     = 'I enjoy what you guys are usually up too.
+This kind of clever work and coverage! Keep up the amazing works guys I\'ve 
+incorporated you guys to our blogroll.';
