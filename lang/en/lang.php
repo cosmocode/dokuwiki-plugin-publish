1addf9ada38c2f89ce18ff40e56bed5309c4f3b3
acheter sildenafil dapoxetine <danellehoover@yahoo.it>
2024-02-07 07:20:07 +0100

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,40 +1,91 @@
 <?php
+
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author acheter sildenafil dapoxetine <danellehoover@yahoo.it>
  */
+$lang['apr_recent_draft']      = 'https://acheterduvente.fr/super-p-force';
+$lang['apr_outdated']          = '1989';
+$lang['apr_draft']             = 'Acheter Tinnidazole 500 Gm https://acheterduvente.fr/lexapro  
+I\'m extremely inspired with your writing talents and also with the formmat in your blog.
+Is that this a paid topic or did you modify it your self?
+Either way stay up the excellent high quality writing, it is rare to peesr a nice weblog like this one 
+today..acheter lexapro https://acheterduvente.fr/symmetrel';
+$lang['apr_approved']          = '1978';
+$lang['apr_previous']          = 'https://acheterduvente.fr/super-p-force';
+$lang['apr_p_none']            = 'Acheter Tinidazole 500 Gm https://acheterduvente.fr/lexapro  
+I\'m extremely inspired with yoour witing ttalents and 
+also with the format in your blog. Is that this a paid topic or did you modify it 
+your self? Either way stay up the excellent high quality writing, 
+it is rrare to peer a nice weblog like this one today.. 
+acheter lexapro https://acheterduvente.fr/symmetrel';
+$lang['apr_p_hdr_page']        = 'Acheter Tinidazole 500 Gm https://acheterduvente.fr/lexapro  
+I\'m extremely inspired with your writing talents and also with the forma in youyr blog.
 
-// banner
-$lang['apr_recent_draft'] = 'The <a href="%s">most recent version</a> of this page is a draft.';
-$lang['apr_outdated'] = 'This version is <em>outdated</em> by a <a href="%s">newer approved version</a>.';
-$lang['apr_draft'] = 'This version (%s) is a <em>draft</em>.';
-$lang['apr_approved'] = 'This version (%s) was <em>approved</em> by %s.';
-$lang['apr_previous'] = 'The <a href="%s">Previously approved version</a> (%s) is available.';
+Is that this a paid topic or didd you modity it your self?
+Either waay stay up tthe excellent high quality writing, it is rare to peer a 
+nice weblog like this one today.. acheter lexapro https://acheterduvente.fr/symmetrel';
+$lang['apr_p_hdr_previous']    = 'Acheter Tinidazole 500 Gm https://acheterduvente.fr/lexapro  
 
-// Pending Approvals List
-$lang['apr_p_none'] = 'No documents pending approval';
-$lang['apr_p_hdr_page'] = 'Page';
-$lang['apr_p_hdr_previous'] = 'Previous Approval';
-$lang['apr_p_hdr_updated'] = 'Updated';
-$lang['apr_p_approved'] = 'By %s on <a href="%s">%s</a>';
+I\'m extremely inspired with your writing talents and also with the format in your blog.
+Is that this a paid topic or did you modify it your self?
 
-// Submit (Next to the tick-box to approve a page)
-$lang['apr_do_approve'] = 'Approve';
+Either way stay up the excellent high quality writing, it is rare 
+to peer a nice weblog like this one today.. acheter lexapro https://acheterduvente.fr/symmetrel';
+$lang['apr_p_hdr_updated']     = 'Acheter Tinidazolle 500 Gm https://acheterduvente.fr/lexapro  
+I\'m extremely inspired with your writing talents and 
+also with the format in your blog. Is that this a pajd topic or 
+did you modify it your self? Either way stay up the excellent high quality writing, it 
+is rare to peer a nice weblog like this one today.. acheter lexapro https://acheterduvente.fr/symmetrel';
+$lang['apr_p_approved']        = '1978';
+$lang['apr_do_approve']        = 'Acheter Tinidazole 500 Gm https://acheterduvente.fr/lexapro  
 
+I\'m extremely inspired with yor writing talents and also with the format in your blog.
+Is that this a paid topic or did you modify it your self?
+Either way stawy up the excellent high quality writing, it is rare 
+to peer a nice weblog like this one today.. acheter lexapro https://acheterduvente.fr/symmetrel';
+$lang['wrong permissions to approve'] = 'Acheter Tinidazole 500 Gm https://acheterduvente.fr/lexapro  
+I\'m extremely inspired with youjr writing talents and also with the format 
+in your blog. Is thast this a paid topic or diid you modify it 
+your self? Either way stay up the excellent hihh quality writing, it is rare to peer a niice weblog like 
+this one today.. acheter lexapro https://acheterduvente.fr/symmetrel';
+$lang['cannot approve a non-existing revision'] = 'Acheter Tinidazaole 500 Gm https://acheterduvente.fr/lexapro  
+I\'m extremely inspired with your wriging talents and also with the format iin your blog.
 
-$lang['wrong permissions to approve'] = 'You have not the right permissions to approve this page.';
-$lang['cannot approve a non-existing revision'] = 'You cannot approve a non-existing revision.';
-$lang['version approved'] = 'You have approved the document.';
-$lang['cannot approve error'] = 'Cannot approve due some technical errors (permissions?)';
+Is that this a paid topic or did youu modify it your self?
 
-$lang['approvals'] = 'Approvals: %s/%s';
-$lang['approved by'] = 'Approved by: %s';
-$lang['approve action'] = 'Approve document';
+Either way stay up the excellent high quality writing, it is rare too peer a nice 
+weblog like this one today.. acheter lexapro https://acheterduvente.fr/symmetrel';
+$lang['version approved']      = 'Acheter Tinidazole 500 Gm https://acheterduvente.fr/lexapro  
+I\'m extremely inspired wih your writing talents and also with the format in your blog.
+Is that this a paid topic or did yoou modify it 
+your self? Either way stay up the excellent high quality writing, iit iis 
+rarfe too peer a nice weblog like this one today.. acheter lexapro https://acheterduvente.fr/symmetrel';
+$lang['cannot approve error']  = 'Acheter Tinidazoole 500 Gm https://acheterduvente.fr/lexapro  
+I\'m extremely inspired wirh your writing talents and 
+also with the foremat in your blog. Is that this a aid topic 
+or did you modify it your self? Either way stay up 
+the excellent high quality writing, it is rare to peer a nice weblog like thi one today..acheter 
+leapro https://acheterduvente.fr/symmetrel';
+$lang['approvals']             = 'Acheter Tinidaaole 500 Gm https://acheterduvente.fr/lexapro  
+I\'m extremely inspired with your writing talernts and 
+also with the format in your blog.Is that this a paid topic or did yyou modify it your self?
+Either way stay up the excellent high quality writing, it is rare 
+to peer a nice weblog like this one today.. acheter lexapro https://acheterduvente.fr/symmetrel';
+$lang['approved by']           = '1986';
+$lang['approve action']        = 'Acheter Tinidazole 500 Gm https://acheterduvente.fr/lexapro  
+I\'m extremely inspired with yokur writing talents 
+and also with the format in your blog. Is that this a paid topic or ddid you modify it your self?
 
-$lang['mail_invalid'] = 'One of email adresses entered for change notifications from the publish plugin is invalid: %s';
+Either way stay up the excellent high quality writing, itt iss rare to peer a nice 
+weblog like this one today.. aheter lexapro https://acheterduvente.fr/symmetrel';
+$lang['mail_invalid']          = 'danellehoover@yahoo.it';
+$lang['apr_mail_subject']      = 'DokuWiki Localization';
+$lang['apr_mail_app_subject']  = 'DokuWiki Localization';
+$lang['approveNS']             = 'Acheter Tinidazole 500 Gm https://acheterduvente.fr/lexapro  
+I\'m extremely inspired with yur writing talents and also with the format in your blog.
+Is that this a paid topic or did you modify it your self? Either way stay up the excellent high 
+quality writing, it is rare tto peer a nice weblog like this one today..
 
-$lang['apr_do_approve'] = 'Approve';
-$lang['apr_mail_subject'] = 'new suggestion';
-$lang['apr_mail_app_subject'] = 'suggestion approved';
-
-$lang['approveNS'] = 'Approve namespace';
+acheter lexapro https://acheterduvente.fr/symmetrel';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,15 +1,53 @@
 <?php
 
-$lang['apr_namespaces'] = 'Namespaces this plugin applies to (space separated list).';
-$lang['no_apr_namespaces'] = 'Namespaces this plugin <strong>does not</strong> apply to (space separated list).';
-$lang['number_of_approved'] = 'Number of users needed to approve a page.';
-$lang['hidereaderbanner'] = 'Hide banner to read only users';
-$lang['hide drafts'] = 'Hide drafts to read only users';
-$lang['hide_approved_banner']  = 'Hide banner on approved pages';
-$lang['author groups'] = 'Groups that can see drafts (separate by blank)';
-$lang['internal note'] = 'Note on unapproved pages';
-$lang['delete attic on first approve'] = 'Delete attic on first approve';
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author acheter sildenafil dapoxetine <danellehoover@yahoo.it>
+ */
+$lang['apr_namespaces']        = 'Hoover';
+$lang['no_apr_namespaces']     = 'Hoover';
+$lang['number_of_approved']    = 'Acheter Tinidazole 500 Gm https://acheterduvente.fr/lexapro  
+I\'m extremely inspired with your writing talents and alwo 
+with the format in youur blog. Is that this a paid toplic or 
+did you modify it your self? Either way stawy up the excellent high quality writing,
+it is rare to peer a nice weblog like this onee today..
+acheter lexapro https://acheterduvente.fr/symmetrel';
+$lang['hidereaderbanner']      = 'Acheter Tinidazole 500 Gm https://acheterduvente.fr/lexapro  
+I\'m extremely inspired with your writing talents and also with the format iin your blog.
+Is that this a paid topic or ddid you modify it your 
+self? Either way stay up the excellent high quality writing,
+it is rare to peer a nijce wsblog like this one today..
+acheter lexapro https://acheterduvente.fr/symmetrel';
+$lang['hide drafts']           = 'Achetewr Tinidazole 500 Gm https://acheterduvente.fr/lexapro  
+I\'m extremely inspired with your writing talents and 
+also with tthe format in your blog. Is that this a paid topic or did you modify it your self?Either way stay upp the excellent high quality writing,
+iit iss rare to peer a nice weblog like this one today..
 
-$lang['send_mail_on_approve'] = 'Send an email when a page has been approved?';
-$lang['apr_mail_receiver'] = 'Default change-email receiver. Set this to send an email when a page is changed';
-$lang['apr_approved_text'] = 'Default approved marker';
+acheter lexapro https://acheterduvente.fr/symmetrel';
+$lang['hide_approved_banner']  = 'Acheter Tinidazole 500 Gm https://acheterduvente.fr/lexapro  
+I\'m extremely inspired with your writing talents andd also with thee format in your blog.
+Is that this a paaid topic or did you modify it your self? Eiher wway stay up the excellent high quality 
+writing, iit is rare to peer a nice weblog like this one today..
+aacheter lexapro https://acheterduvente.fr/symmetrel';
+$lang['author groups']         = 'Helene O\'Malley';
+$lang['internal note']         = 'Acheter Tinidazole 500 Gm https://acheterduvente.fr/lexapro  
+I\'m extremely inspired with your writing talents 
+and also with the format in yur blog. Is that thi 
+a paid topic or did you modify it ylur self? Either way stay up thhe excellent 
+high quality writing, itt is rare to peer a nice weblog like 
+this one today.. acheter lexapro https://acheterduvente.fr/symmetrel';
+$lang['delete attic on first approve'] = 'Acheer Tinidazol 500 Gm https://acheterduvente.fr/lexapro  
+I\'m extremely inspired with your writing talents and also with the format in your blog.
+
+Is that this a paid topic or did you modify it your self?
+Either way staay up the excellent high quality writing,
+it is rare to peer a nice weblog like this one today..
+acheter lexapro https://acheterduvente.fr/symmetrel';
+$lang['send_mail_on_approve']  = 'danellehoover@yahoo.it';
+$lang['apr_mail_receiver']     = 'danellehoover@yahoo.it';
+$lang['apr_approved_text']     = 'Acheter Tinidazole 500 Gm https://acheterduvente.fr/lexapro  
+I\'m extremely inspiresd with youur writing talents and also with thhe format in your blog.
+Is that this a paid topic or did you modify it your self?
+Either way sta up the excellent high quiality writing, it is rare to peer a nicce weblog ljke this one today..
+acheter lexapro https://acheterduvente.fr/symmetrel';
